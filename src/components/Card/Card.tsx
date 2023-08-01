63335ca7c6d6b5b6a760ff15498cf8a96532ec03
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-08-01 19:50:06 +0530

    feat: Added styles for dragging events

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import styles from "./Card.module.css";
 
 function Card() {
+  const [isDragged, setIsDragged] = useState(false);
+
+  const handleCardDrag = () => {
+    setIsDragged(true);
+  };
+
+  const handleCardDragLeave = () => {
+    setIsDragged(false);
+  };
+
   return (
-    <div className={styles.Card} draggable>
+    <div
+      className={`${styles.Card} ${isDragged ? styles.cardDragging : ""}`}
+      draggable
+      onDrag={handleCardDrag}
+      onDragEnd={handleCardDragLeave}
+    >
       Card
     </div>
   );
--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 import Card from "../Card/Card";
 import styles from "./CardList.module.css";
 
 function CardList() {
+  const [isDraggedOver, setIsDraggedOver] = useState(false);
+
+  const handleDragOver = () => {
+    setIsDraggedOver(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDraggedOver(false);
+  };
+
   return (
-    <div className={styles.CardList}>
+    <div
+      className={`${styles.CardList} ${
+        isDraggedOver ? styles.draggedOver : ""
+      }`}
+    >
       <h2>Todo</h2>
-      <div className={styles.CardsContainer}>
+      <div
+        className={styles.CardsContainer}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+      >
         <Card />
         <Card />
         <Card />
